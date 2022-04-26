32143571d306ebc7f3352eb9d6ab2b7b2016b1e4
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-04-26 13:39:52 -0700

    Finds past data

--- DiscountedCashFlow.py
+++ DiscountedCashFlow.py
@@ -18,14 +18,24 @@ def pull_data(ticker):
     enterpriseValue = get_jsonparsed_data(enterpriseValueUrl)
     incomeStatementURl = "https://financialmodelingprep.com/api/v3/income-statement/" + ticker + "?limit=120&apikey=82e32bddd70d4deecdf507b9a13b2867"
     incomeStatement = get_jsonparsed_data(incomeStatementURl)
-    currentPriceURL = "https://financialmodelingprep.com/api/v3/quote-short/" + ticker + "?apikey=82e32bddd70d4deecdf507b9a13b2867"
-    currentPrice = get_jsonparsed_data(currentPriceURL)
+    sharePriceURL = "https://financialmodelingprep.com/api/v3/historical-price-full/" + ticker + "?serietype=line&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    sharePrice = get_jsonparsed_data(sharePriceURL)
+
+    # What I can do is subtract today's date from the date in the income statement and find that index
+    '''
+    for i in range(sharePrice)
+        for key, value in data.items():
+            if key == "date":
+                for key, value in value.items():
+                    if value != None:
+                        metrics["date"] = value
+    '''
 
     df1 = pd.DataFrame([balanceSheet[0]])
     df2 = pd.DataFrame([outstandingShares[0]])
     df3 = pd.DataFrame([enterpriseValue[0]])
     df4 = pd.DataFrame([incomeStatement[0]])
-    df5 = pd.DataFrame([currentPrice[0]])
+    df5 = pd.DataFrame([sharePrice[0]])
     data = pd.concat([df1, df2, df3, df4, df5])
     data.reset_index(drop=True, inplace=True)
 
@@ -36,13 +46,14 @@ def pull_data(ticker):
 
 def isolate_data(data):
     metrics = {}
+
+    # For current year
     for key, value in data.items():
-        #print(key, ':', value)
         if key == "ebitda":
             for key, value in value.items():
                 if value != None:
                     metrics["ebitda"] = value
-        elif key == "price":
+        elif key == "close": # Open the "historical" array, search through the dictionary
             for key, value in value.items():
                 if value != None:
                     metrics["price"] = value
@@ -62,13 +73,52 @@ def isolate_data(data):
             for key, value in value.items():
                 if value != None:
                     metrics["numberOfShares"] = value
+        elif key == "incomeTaxExpense":
+            for key, value in value.items():
+                if value != None:
+                    metrics["incomeTaxExpense"] = value
+        elif key == "totalCurrentAssets":
+            for key, value in value.items():
+                if value != None:
+                    metrics["totalCurrentAssets"] = value
+        elif key == "totalCurrentLiabilities":
+            for key, value in value.items():
+                if value != None:
+                    metrics["totalCurrentLiabilities"] = value
+
+    # For year previous
+
     return metrics
 
 def computations(metrics):
-    computations{}
-    #NOPLAT = (Net Income + Tax + Interest + Non-Operating Gains/Losses) * (1 - (totalTaxExpenses/ebita))
-    #computations["NOPLAT"] = NOPLAT
-    return
+    computations = {}
+
+    # This value is inflated because I am using ebitda instead of ebita
+    NOPLAT = (metrics["ebitda"]) * (1 - (metrics["incomeTaxExpense"] / metrics["ebitda"]))
+    computations["NOPLAT"] = NOPLAT
+    print("NOPLAT :", str(computations["NOPLAT"]))
+
+    investedCapital = (metrics["totalCurrentAssets"] - metrics["totalCurrentLiabilities"])
+    computations["investedCapital"] = investedCapital
+    print("investedCapital :", str(computations["investedCapital"]))
+
+    investedCapitalPrevYear = (metrics["totalCurrentAssetsPrevYear"] - metrics["totalCurrentLiabilitiesPrevYear"])
+    computations["investedCapitalPrevYear"] = investedCapitalPrevYear
+    print("investedCapitalPrevYear :", str(computations["investedCapitalPrevYear"]))
+
+    investedCapitalOverRevenue = computations["investedCapital"] / metrics["revenue"]
+    computations["investedCapitalOverRevenue"] = investedCapitalOverRevenue
+    print("investedCapitalOverRevenue :", str(computations["investedCapitalOverRevenue"]))
+
+    newNetInvestment = computations["investedCapital"] - computations["investedCapitalPrevYear"]
+    computations["newNetInvestment"] = newNetInvestment
+    print("newNetInvestment :", str(computations["newNetInvestment"]))
+
+    operatingFreeCashFlow = computations["NOPLAT"] - computations["newNetInvestment"]
+    computations["operatingFreeCashFlow"] = operatingFreeCashFlow
+    print("operatingFreeCashFlow :", str(computations["operatingFreeCashFlow"]))
+
+    return computations
 
 # Sourced from Financial Modeling Prep
 def get_jsonparsed_data(url):
