9020b31d9d192799456cdbbc2dd2dde6db2782e8
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-04-26 19:31:19 -0700

    Load past data

--- DiscountedCashFlow.py
+++ DiscountedCashFlow.py
@@ -2,6 +2,20 @@
 Author: Teeron Hajebi Tabrizi
 
 Description:
+The aim of this program will be to calculate the expected value of a company’s shares
+using the Discounted Cash Flow (DCF) method. DCF’s have long been utilized by traders
+and bankers to evaluate the expected price of a given stock using financial data.
+Although they are generally used to complement industry and market knowledge, it allows
+one to see the intrinsic value of a company. It can also help when trying to find out
+whether a company is over or undervalued. This project will act as a way for me to further
+learn about how to implement and use APIs. Financial Modeling Prep has a free API that
+lets me pull a company’s various financial metrics. It will mostly be data science oriented
+as I will have to fetch and clean vast amounts of information, but I may be able to
+incorporate some machine learning by creating projections to estimate the valuation at a
+later point in time. If I am able to fulfill some of my reach goals, this application will
+serve as a way to find undervalued stocks to purchase or overvalued shares to shares through
+the use of the initial DCF model and other metrics.
+
 '''
 
 import json
@@ -18,25 +32,12 @@ def pull_data(ticker):
     enterpriseValue = get_jsonparsed_data(enterpriseValueUrl)
     incomeStatementURl = "https://financialmodelingprep.com/api/v3/income-statement/" + ticker + "?limit=120&apikey=82e32bddd70d4deecdf507b9a13b2867"
     incomeStatement = get_jsonparsed_data(incomeStatementURl)
-    sharePriceURL = "https://financialmodelingprep.com/api/v3/historical-price-full/" + ticker + "?serietype=line&apikey=82e32bddd70d4deecdf507b9a13b2867"
-    sharePrice = get_jsonparsed_data(sharePriceURL)
-
-    # What I can do is subtract today's date from the date in the income statement and find that index
-    '''
-    for i in range(sharePrice)
-        for key, value in data.items():
-            if key == "date":
-                for key, value in value.items():
-                    if value != None:
-                        metrics["date"] = value
-    '''
 
     df1 = pd.DataFrame([balanceSheet[0]])
     df2 = pd.DataFrame([outstandingShares[0]])
     df3 = pd.DataFrame([enterpriseValue[0]])
     df4 = pd.DataFrame([incomeStatement[0]])
-    df5 = pd.DataFrame([sharePrice[0]])
-    data = pd.concat([df1, df2, df3, df4, df5])
+    data = pd.concat([df1, df2, df3, df4])
     data.reset_index(drop=True, inplace=True)
 
     # Sourced from Stack Overflow
@@ -44,19 +45,57 @@ def pull_data(ticker):
     dt = open(ticker + "Data.json")
     return json.load(dt)
 
+def pull_data_prev_years(ticker):
+    # Sourced from Financial Modeling Prep
+    balanceSheetUrl = "https://financialmodelingprep.com/api/v3/balance-sheet-statement/" + ticker + "?limit=120&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    balanceSheet = get_jsonparsed_data(balanceSheetUrl)
+    outstandingSharesUrl = "https://financialmodelingprep.com/api/v4/shares_float?symbol=" + ticker + "&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    outstandingShares = get_jsonparsed_data(outstandingSharesUrl)
+    enterpriseValueUrl = "https://financialmodelingprep.com/api/v3/enterprise-values/" + ticker + "?limit=40&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    enterpriseValue = get_jsonparsed_data(enterpriseValueUrl)
+    incomeStatementURl = "https://financialmodelingprep.com/api/v3/income-statement/" + ticker + "?limit=120&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    incomeStatement = get_jsonparsed_data(incomeStatementURl)
+
+    df1 = pd.DataFrame([balanceSheet[1]])
+    df2 = pd.DataFrame([enterpriseValue[1]])
+    df3 = pd.DataFrame([incomeStatement[1]])
+    data = pd.concat([df1, df2, df3])
+    data.reset_index(drop=True, inplace=True)
+
+    # Sourced from Stack Overflow
+    data.to_json(ticker + "Data(YearPrev).json")
+    dt = open(ticker + "Data(YearPrev).json")
+    return json.load(dt)
+
+def isolate_data_prev_year(prevData):
+    prevMetrics = {}
+
+    # For year previous
+    for key, value in prevData.items():
+        if key == "totalCurrentAssets":
+            for key, value in value.items():
+                if value != None:
+                    prevMetrics["totalCurrentAssets"] = value
+        elif key == "totalCurrentLiabilities":
+            for key, value in value.items():
+                if value != None:
+                    prevMetrics["totalCurrentLiabilities"] = value
+
+    return prevMetrics
+
 def isolate_data(data):
     metrics = {}
 
     # For current year
     for key, value in data.items():
-        if key == "ebitda":
+        if key == "date":
+            for key, value in value.items():
+                if value != None:
+                    metrics["date"] = value
+        elif key == "ebitda":
             for key, value in value.items():
                 if value != None:
                     metrics["ebitda"] = value
-        elif key == "close": # Open the "historical" array, search through the dictionary
-            for key, value in value.items():
-                if value != None:
-                    metrics["price"] = value
         elif key == "operatingExpenses":
             for key, value in value.items():
                 if value != None:
@@ -86,11 +125,15 @@ def isolate_data(data):
                 if value != None:
                     metrics["totalCurrentLiabilities"] = value
 
-    # For year previous
+    '''
+    previousMetrics = isolate_data_prev_year(data)
+    combinedMetrics = metrics | previousMetrics
+    return combinedMetrics
+    '''
 
     return metrics
 
-def computations(metrics):
+def computations(metrics, prevMetrics):
     computations = {}
 
     # This value is inflated because I am using ebitda instead of ebita
@@ -102,7 +145,8 @@ def computations(metrics):
     computations["investedCapital"] = investedCapital
     print("investedCapital :", str(computations["investedCapital"]))
 
-    investedCapitalPrevYear = (metrics["totalCurrentAssetsPrevYear"] - metrics["totalCurrentLiabilitiesPrevYear"])
+    # Review computations
+    investedCapitalPrevYear = (prevMetrics["totalCurrentAssets"] - prevMetrics["totalCurrentLiabilities"])
     computations["investedCapitalPrevYear"] = investedCapitalPrevYear
     print("investedCapitalPrevYear :", str(computations["investedCapitalPrevYear"]))
 
@@ -126,11 +170,33 @@ def get_jsonparsed_data(url):
     data = response.read().decode("utf-8")
     return json.loads(data)
 
+def share_price(ticker, metrics):
+
+    sharePriceURL = "https://financialmodelingprep.com/api/v3/historical-price-full/" + ticker + "?serietype=line&apikey=82e32bddd70d4deecdf507b9a13b2867"
+    sharePrice = get_jsonparsed_data(sharePriceURL)
+
+    # Create a range of possible dates as the API doesn't have every single day
+    # The range is between two days (+/-) of the income statement's release
+    date = metrics["date"]
+    dateRange = [str(date[:7] + str(int(date[7:]) + 1)), str(date[:7] + str(int(date[7:]) - 1)), str(date[:7] + str(int(date[7:]) + 2)), str(date[:7] + str(int(date[7:]) - 2))]
+
+    for key, value in sharePrice.items():
+        if key == "historical":
+            for index in range(len(value)):
+                if value[index]["date"] in dateRange:
+                    print("Value on", value[index]["date"], ":", value[index]["close"])
+                    return value[index]["close"]
+
+def compare(valuation, sharePrice):
+    return
+
 if __name__ == "__main__":
     ticker = input("Input ticker: ")
     data = pull_data(ticker)
+    prevData = pull_data_prev_years(ticker)
     metrics = isolate_data(data)
-    valuation = computations(metrics)
-
-    for key, value in metrics.items():
-        print(key, ':', value)
+    prevMetrics = isolate_data_prev_year(prevData)
+    computations = computations(metrics, prevMetrics)
+    #valuation = valuation(computations())
+    sharePrice = share_price(ticker, metrics)
+    #compare(valuation, sharePrice)
